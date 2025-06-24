3659641d87ab53b84d147f968bdb7e5100bb612c
Maciej Majerczyk <maciej.majerczyk@jumio.com>
2025-06-24 15:00:52 +0200

    Create endpoint for creation of the user

--- app/database.py
+++ app/database.py
@@ -2,7 +2,6 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 import os
 
-
 DATABASE_URL = os.environ.get("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set")
@@ -11,5 +10,9 @@ engine = create_async_engine(DATABASE_URL, echo=True)
 
 AsyncSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
 
-
 Base = declarative_base()
+
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from . import models
-from .database import engine
+from .database import engine, get_db
+from .schemas import UserRequest, UserResponse
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
 
 app = FastAPI()
 
@@ -14,3 +19,17 @@ async def on_startup():
 @app.get("/")
 async def root():
     return {"message": "Hello async FastAPI + PostgreSQL"}
+
+
+@app.post("/users/", response_model=UserResponse)
+async def create_user(user: UserRequest, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(models.User).where(models.User.email == user.email))
+    existing_user = result.scalars().first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    await db.commit()
+    await db.refresh(new_user)
+    return new_user
